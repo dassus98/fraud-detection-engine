6db0dba60a34fa80b3381723aac448d6b8e966ef
Chitra <johncdas1998@gmail.com>
2026-02-02 22:51:33 -0500

    similar edits

--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -15,6 +15,7 @@ class FraudPipeline:
         self.manual_selection = selected_features or [
             'C1', 'C2', 'C5', 'C8', 'C9', 'C12', # Choosing Cs with >0.30 correlation with Fraud
             'D1', 'D3', 'D4', 'D5', 'D8', 'D10', 'D11', 'D13', 'D14', 'D15', # Choosing non-collinear D variables
-            'M1', 'M4', 'M5', 'M6', 'M7'
+            'M1', 'M4', 'M5', 'M6', 'M7',
+            
 
         ]
